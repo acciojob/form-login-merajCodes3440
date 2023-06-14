23e00bd22da0048d0e6bb5db50b49002b632654c
MerajCodes <113118915+merajCodes3440@users.noreply.github.com>
2023-06-14 17:57:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	 let fname =document.getElementsByTagName('input')[0];
+     let lname =document.getElementsByTagName('input')[1];
+     alert(fname.value+" "+lname.value);
 
 }
